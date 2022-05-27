aa2186d85a5102fcae8f3d35ee5432070ae52df7
Steven Mi <steven.mi@getyourguide.com>
2022-05-27 16:58:11 +0200

    chore: replace ending tag

--- components/footer.tsx
+++ components/footer.tsx
@@ -30,8 +30,8 @@ export default function Footer() {
             icon: (props: any) => (
                 <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
                     <path stroke="none"
-                          d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
-                    <circle cx="4" cy="4" r="2" stroke="none"></circle>
+                          d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"/>
+                    <circle cx="4" cy="4" r="2" stroke="none"/>
                 </svg>
             ),
         },
